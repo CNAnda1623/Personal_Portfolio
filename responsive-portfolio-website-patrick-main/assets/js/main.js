ca2edde723ec30bf07ddb5e1d9debd9d13790656
CNanda162 <154887193+CNanda162@users.noreply.github.com>
2024-12-25 11:31:26 +0530

    New commit

--- responsive-portfolio-website-patrick-main/assets/js/main.js
+++ responsive-portfolio-website-patrick-main/assets/js/main.js
@@ -50,7 +50,8 @@ const sendEmail = (e) =>{
         //show sent message
         contactMessage.textcontent = 'Message Sent Successfully✅'
     }, ()=>{
-        
+        // show error message
+        contactMessage.textcontent = 'Message not sent (service error)❌'
     })
 }
 
@@ -58,9 +59,46 @@ contactForm.addEventListener('submit', sendEmail)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true // Animation repeat
+})
+
+sr.reveal('.home__data, .home__social, .contact__container, .footer__container')
+sr.reveal('.home__image', {origin: 'bottom'})
+sr.reveal('.about__data, .skills__data', {origin: 'left'})
+sr.reveal('.about__image, .skills__content', {origin: 'right'})
+sr.reveal('.services__card, .projects__card', {Interval:100})
